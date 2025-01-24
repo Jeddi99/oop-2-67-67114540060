dc2e49f8ce88f0a6e8e693f13d6a7cb4ecd476ba
JeddilokSudnert <sudnert12@gmail.com>
2025-01-24 14:03:47 +0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+class std(ob)
+ std_count = 0
+  
+  def _new_(self):
+    print('std._new_Z')
+  
+  def _init_(self):
+    print('std._init_')
+  
+  def study_hard(self):
+    print("sir,yes sir.")
